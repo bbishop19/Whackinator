3a52ae778670977589daf77aa429621401362766
bbishop19 <bbishop19@windwardschool.org>
2019-03-03 11:30:39 -0800

    mostly finish project

--- src/BinaryTreeHelper.java
+++ src/BinaryTreeHelper.java
@@ -1,23 +1,24 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Stack;
 
 public abstract class BinaryTreeHelper {
-    public String serialize(BTNode head){
+    public static String serialize(BTNode head){
         return (head.getData() + ", " +
-                head.getLeftChild()==null?"n, ":serialize(head.getLeftChild())+
-                (head.getRightChild()==null?"n, ":serialize(head.getRightChild());
+                (head.getLeftChild()==null?"n, ":serialize(head.getLeftChild()))+
+                (head.getRightChild()==null?"n, ":serialize(head.getRightChild())));
     }
 
 
-    public BTNode deserialize(ArrayList<String> arr){
-        String s = arr.get(0);
-        arr.remove(0);
-        if(s=="n"){
+    public static BTNode deserialize(LinkedList<String> arr){
+        String s = arr.remove(0);
+        if(s.equals("n")){
             return null;
         }
         BTNode head = new BTNode(null, null, s);
         head.setLeftChild(deserialize(arr));
         head.setRightChild(deserialize(arr));
+        return head;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,89 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String string = "";
+        try {
+            string = new String(Files.readAllBytes(Paths.get(Main.class.getClassLoader().getResource("CerealTree.txt").getPath())), "UTF-8");
+        }catch(IOException e){}
+        //copied/modified from stack overflow
+        //https://stackoverflow.com/questions/326390/how-do-i-create-a-java-string-from-the-contents-of-a-file
+
+        LinkedList<String> serializedTree = new LinkedList<>(Arrays.asList(string.trim().split(", ")));
+
+        BTNode curr = BinaryTreeHelper.deserialize(serializedTree);
+        Scanner Systemdotin = new Scanner(System.in);
+        /////////////////////////////////////////////////////////////////////
+        help();
+
+        while(curr.getLeftChild() != null & curr.getRightChild() != null){
+            System.out.println(curr.getData());
+
+            String response = Systemdotin.nextLine();
+
+            switch(response) {
+                case "y":
+                    curr = curr.getLeftChild();
+                    break;
+                case "n":
+                    curr = curr.getRightChild();
+                    break;
+                case "--help":
+                    help();
+                    break;
+                default:
+                    System.out.println("\nUnexpected response, please try again");
+            }
+            System.out.println();
+
+
+        }
+
+        boolean eternalPrison = true;
+        while(eternalPrison) {
+            System.out.println("Are you thinking of " + curr.getData() + "?");
+
+            String response = Systemdotin.nextLine();
+
+            switch (response) {
+                case "y":
+                    eternalPrison = false;
+                    System.out.println("\nThank you for playing Wackinator");
+                    break;
+                case "n":
+                    eternalPrison = false;
+                    System.out.println("\nWho are you thinking of?");
+                    response = Systemdotin.nextLine();
+                    try {//TODO This doesnt work, please fix
+                        Files.write(Paths.get(Main.class.getClassLoader().getResource("NewNames.txt").getPath()), response.getBytes(), StandardOpenOption.APPEND);
+                    } catch (IOException e) {System.out.println("oopsie");}
+                    break;
+                case "--help":
+                    help();
+                    break;
+                default:
+                    System.out.println("\nUnexpected response, please try again\n");
+            }
+        }
+
+    }
+
+
+    static void help(){
+        System.out.println("Welcome to Wackinator (Windward Akinator)");
+        System.out.println("To use this program, first think of a Windward teacher");
+        System.out.println("Then, respond to questions with either \"y\" for yes, \"n\" for no");
+        System.out.println("If you need help, just type \"--help\"");
+        System.out.println("Good luck!\n");
     }
 }
+
